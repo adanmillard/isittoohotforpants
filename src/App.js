31a339790138015249ee340ae55569467a16d437
Adan Millard <adanmillard@gmail.com>
2023-02-11 13:29:27 +1300

    firebase config

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const getCityWeather = () => {
     axios
       .get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${userCityInput},${userCountyInput}&limit=5&appid=${weatherKey}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${userCityInput},${userCountyInput}&limit=5&appid=${weatherKey}`
       )
       .then((response) => {
         const lat = response.data[0].lat;
