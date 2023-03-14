c2e61192781a698821f200e851cb4d02e77bae02
Adan Millard <adanmillard@gmail.com>
2023-03-14 19:26:03 +1300

    testing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div>
       <div>
-        <p>Is it too hot for pants. Created by Adan Millard 2023</p>
+        <p>Is it too hot for pants. Created by Adan Millard 2023.</p>
       </div>
     </div>
   );
