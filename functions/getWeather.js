7230ff171282311a601e98885c89c243bed22983
Adan Millard <adanmillard@gmail.com>
2023-04-04 19:11:58 +1200

    cors

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -7,6 +7,10 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
     body: JSON.stringify(response.data),
   };
 };
