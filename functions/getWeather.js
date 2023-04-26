cee5c58da42992c0cc1bd4c17ba91d307eac138e
Adan Millard <adanmillard@gmail.com>
2023-04-26 19:27:56 +1200

    changed fetch method to axios while trying to sort serverless function

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -4,12 +4,12 @@ const cors = require("cors");
 exports.handler = cors({ origin: "*" })(async function (event, context) {
   const { lat, lng } = event.queryStringParameters;
 
-  // if (!lat || lat.trim() === "") {
-  //   return {
-  //     statusCode: 400,
-  //     body: "Please provide a valid city",
-  //   };
-  // }
+  if (!lat || lat.trim() === "") {
+    return {
+      statusCode: 400,
+      body: "Please provide a valid city",
+    };
+  }
 
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${process.env.WEATHER_API_KEY}`;
