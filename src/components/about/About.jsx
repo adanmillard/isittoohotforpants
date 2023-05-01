de1b3cbfae77916ae41c94cddc2c19f097904dcf
Adan Millard <adanmillard@gmail.com>
2023-05-02 05:24:45 +1200

    created model for about section of the site

---
+++ src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../about/about.css";
+
+export const About = ({ setOpen }) => {
+  return (
+    <div className="about-container">
+      <div className="about-content-style">
+        <button className="close-button" onClick={() => setOpen(false)}>
+          &times;
+        </button>
+        <article>
+          <p>
+            The origin of this web/app came to me after searching trying to see
+            if it was too hot to wear pants, after googling all that came back
+            was buzzfeed articles, I just wanted to know if it was too hot /
+            cold to wear pants!
+          </p>
+          <p>Thus, is it too hot for pants? was born.</p>
+          <p>
+            A fun app to decide <b>WEATHER</b> or not you should wear long
+            pants.
+          </p>
+          <p>
+            Hopefully this has solved your problem of trying to decided if you
+            should wear pants or shorts, long dress or short skirt.
+          </p>
+          <p>
+            Disclaimer: As a Kiwi I refer to pants as, long pants, trousers,
+            jeans, etc..
+          </p>
+        </article>
+      </div>
+    </div>
+  );
+};
--- src/components/header/Heading.js
+++ src/components/header/Heading.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "../header/heading.css";
+import { useState } from "react";
+import { About } from "../about/About";
 
 export const Heading = () => {
+  const [open, setOpen] = useState(false);
+
+  const openAbout = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="header-container">
       <header>
-        <h1>Is it too hot for pants?</h1>
-        <p>
-          A fun app to decide <b>WEATHER</b> or not you should wear long pants.
-          Type in your city below to find out!
-        </p>
+        <div>
+          <h1>Is it too hot for pants?</h1>
+        </div>
+        <div>
+          <button onClick={openAbout} className="about-button">
+            <h1>About</h1>
+          </button>
+          {open ? <About setOpen={setOpen} /> : null}
+        </div>
       </header>
     </div>
   );
